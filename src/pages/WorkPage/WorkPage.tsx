f377815d5893b9ab11cc8ecd17c48de03fdfedf5
dmtr636 <dmtr636@gmail.com>
2023-06-17 15:45:13 +0300

    New version

--- src/pages/WorkPage/WorkPage.tsx
+++ src/pages/WorkPage/WorkPage.tsx
@@ -1,17 +1,29 @@
 import { useParams } from "react-router-dom";
 import { store } from "@/stores/store.ts";
 import { observer } from "mobx-react-lite";
+import styles from "./style.module.scss";
 
 export const WorkPage = observer(() => {
     const params = useParams<{ id: string }>();
+
     let work;
     if (params.id) {
         work = store.getWorkById(params.id);
     }
 
-    if (!work) {
-        return <div>Загрузка...</div>;
+    if (store.works.length && !work) {
+        return <div>Ошибка</div>;
     }
 
-    return <div>{work.name}</div>;
+    return (
+        <div className={styles.layout}>
+            <div className={styles.nameRow}>
+                <div className={styles.name}>{work?.name ?? "Загрузка..."}</div>
+                <div className={styles.type}>{work?.purpose || "STUDY"}</div>
+            </div>
+            <div className={styles.description}>
+                {work?.description}
+            </div>
+        </div>
+    );
 });
